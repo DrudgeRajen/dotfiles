537e73d8c0d0d63abf85872b9a46141af3840cf9
Rajendra Sharma <drudge.rajan@gmail.com>
2025-09-24 09:01:48 +0530

    transparent nvim

--- .config/nvim/lua/drudge/plugins/colorscheme.lua
+++ .config/nvim/lua/drudge/plugins/colorscheme.lua
@@ -7,7 +7,10 @@ return {
   -- gruvbox-material
   {
     "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
     config = function()
+      vim.g.gruvbox_material_transparent_background = 2
       vim.cmd.colorscheme "gruvbox-material"
     end
 
