49516f2e1bae73b6a880522024a2674a91d624d8
Rajendra Sharma <drudge.rajan@gmail.com>
2025-10-07 09:30:09 +0530

    added keymap for vim fugitive

--- .config/nvim/lua/drudge/plugins/vim-fugitive.lua
+++ .config/nvim/lua/drudge/plugins/vim-fugitive.lua
@@ -1,3 +1,8 @@
 return {
-  "tpope/vim-fugitive"
+  "tpope/vim-fugitive",
+  keys = {
+    { "<leader>gs", "<cmd>Git<cr>",        desc = "Git status" },
+    { "<leader>gp", "<cmd>Git push<cr>",   desc = "Git push" },
+    { "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
+  }
 }
